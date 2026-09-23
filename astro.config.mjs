bf4af63a1160bae2d38051c696d555b3840c486c
M.Ihsan <ihsan.onlinefs@gmail.com>
2026-09-23 12:44:25 +0700

    set domain at astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://drinkregen.com/",
+  site: "https://coihsan.github.io/drinkregen/",
 
   output: "static",
 
